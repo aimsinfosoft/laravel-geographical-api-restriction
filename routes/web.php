76e5672ad73818878b5b7005f8540a183e9e1953
dhruvil-aims <dhruvil.patel@Aimsinfosoft.com>
2025-01-06 15:07:03 +0530

    first commit

--- routes/web.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::get('/contact', function () {
-    return \config('georestriction.admin_email');
-});	
--- src/GeoRestrictionServiceProvider.php
+++ src/GeoRestrictionServiceProvider.php
@@ -19,14 +19,11 @@ class GeoRestrictionServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // dd(123);
         $this->publishes([
             __DIR__.'/../config/config.php'=> config_path('georestriction.php')
         ],'georestriction-config' );
 
         $router = $this->app['router'];
         $router->aliasMiddleware('geo.restriction', \Laravel\GeoRestriction\Middleware\GeoRestrictionMiddleware::class);
-
-        $this->LoadRoutesFrom(__DIR__.'/../routes/web.php');
     }
 }
